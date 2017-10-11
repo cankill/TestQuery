694c9526699d1ddf601917abb5862516c8db5940
Andrey Filyanin <Andrey.Filyanin@teligent.ru>
2017-10-11 15:21:45 +0300

    Working version with throtling

--- manual_tests/single.sh
+++ manual_tests/single.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X GET -H "content-type: application/json" "http://localhost:8080/search?tag=scala&tag=clojure"
+curl -X GET -H "content-type: application/json" "http://localhost:8080/search?tag=scala&tag=clojure" &
--- src/main/scala/ru/fan/query/Main.scala
+++ src/main/scala/ru/fan/query/Main.scala
@@ -1,4 +1,6 @@
 package ru.fan.query
+import java.util.concurrent.TimeUnit
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.coding.{Deflate, Gzip, NoCoding}
@@ -8,22 +10,28 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.ExceptionHandler
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream._
-import akka.stream.scaladsl.{Keep, Sink, Source, SourceQueueWithComplete}
+import akka.stream.scaladsl.{Framing, Keep, Sink, Source, SourceQueueWithComplete}
 import com.typesafe.config.ConfigFactory
 import spray.json._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.StatusCodes.{CustomStatusCode, HttpFailure, HttpSuccess}
+import akka.util.ByteString
 
 import scala.collection.immutable
-import scala.concurrent.{ExecutionContextExecutor, Future, Promise}
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContextExecutor, Future, Promise}
 import scala.io.StdIn
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
+import scala.concurrent.duration._
 
 final case class Item(is_answered: Boolean)
 final case class SOReply(tag: Option[String], items: List[Item])
+final case class SOError(error_id: String, error_message: String,error_name: String)
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val itemFormat: RootJsonFormat[Item] = jsonFormat1(Item)
   implicit val apiStatusFormat: RootJsonFormat[SOReply] = jsonFormat2(SOReply)
+  implicit val errorFormat: RootJsonFormat[SOError] = jsonFormat3(SOError)
 }
 
 object Main extends App with JsonSupport {
@@ -35,9 +43,15 @@ object Main extends App with JsonSupport {
 
   private val httpConfig = config.getConfig("http")
   private val queueConfig = config.getConfig("queue")
+  private val throtlingConfig = config.getConfig("throtling")
   lazy val interface = httpConfig.getString("interface")
   lazy val port = httpConfig.getInt("port")
   lazy val queueSize = queueConfig.getInt("size")
+  lazy val elements = throtlingConfig.getInt("elements")
+  lazy val per = throtlingConfig.getDuration("per")
+  lazy val perFinite = FiniteDuration(per.getSeconds, TimeUnit.SECONDS)
+
+  println(queueSize)
 
   def decodeResponse(response: HttpResponse): HttpResponse = {
     val decoder = response.encoding match {
@@ -45,7 +59,7 @@ object Main extends App with JsonSupport {
         Gzip
       case HttpEncodings.deflate ⇒
         Deflate
-      case HttpEncodings.identity ⇒
+      case _ ⇒
         NoCoding
     }
 
@@ -57,6 +71,12 @@ object Main extends App with JsonSupport {
 
   val queue: SourceQueueWithComplete[(HttpRequest, (String, Promise[HttpResponse]))] =
     Source.queue[(HttpRequest, (String, Promise[HttpResponse]))](queueSize, OverflowStrategy.dropNew)
+      .throttle(
+        elements = elements, //number of elements to be taken from bucket
+        per = perFinite,
+        maximumBurst = queueSize,  //capacity of bucket
+        mode = ThrottleMode.shaping
+      )
       .via(poolClientFlow)
       .toMat(Sink.foreach({
         case ((Success(resp), p)) => (p._1, p._2.success(resp))
@@ -64,28 +84,34 @@ object Main extends App with JsonSupport {
       }))(Keep.left)
       .run()
 
-  def queueRequest(tag: String): Future[(String, HttpResponse)] = {
+  def queueRequest(tag: String): Future[JsObject] = {
     val queryParams: Map[String, String] = Map(
       "pagesize" -> "100",
       "order" -> "desc",
       "sort" -> "creation",
       "tagged" -> tag,
-      "site" -> "stackoverflow"
+      "site" -> "stackoverflow",
+      "key" -> "1Hh4m1ExsXrtnD2PJQO1JA(("
     )
     val uri = Uri("/2.2/search").withQuery(Uri.Query(queryParams))
-//    val uri = Uri(s"/2.2/search?pagesize=100&order=desc&sort=creation&tagged=$tag&site=stackoverflow", Uri.ParsingMode.Relaxed)
-
-
-
-//    val uri = Uri(s"/2.2/search?pagesize=100&order=desc&sort=creation&tagged=$tag&site=stackoverflow", Uri.ParsingMode.Relaxed)
     val request = HttpRequest(uri = uri)
 
     val responsePromise = Promise[HttpResponse]()
     queue.offer(request -> (tag, responsePromise)).flatMap {
-      case QueueOfferResult.Enqueued    => responsePromise.future.map(r => (tag, r))
+      case QueueOfferResult.Enqueued    => responsePromise.future.flatMap(r => extractResult((tag, r)))
       case QueueOfferResult.Dropped     => Future.failed(new RuntimeException("Queue overflowed. Try again later."))
       case QueueOfferResult.Failure(ex) => Future.failed(ex)
       case QueueOfferResult.QueueClosed => Future.failed(new RuntimeException("Queue was closed (pool shut down) while running the request. Try again later."))
+    }
+  }
+
+  def extractResult(tuple: (String, HttpResponse)): Future[JsObject] = {
+    val unzippedResponse = decodeResponse(tuple._2)
+
+    if(unzippedResponse.status.isSuccess()) {
+      Unmarshal(unzippedResponse.entity).to[SOReply].map(aps => doCalculate(aps.copy(tag = Some(tuple._1))))
+    } else {
+      Unmarshal(unzippedResponse.entity).to[JsObject]
     }
   }
 
@@ -103,22 +129,12 @@ object Main extends App with JsonSupport {
     )
   }
 
-  def extractResult(tuple: (String, HttpResponse)): Future[JsObject] = {
-    val unzippedResponse = decodeResponse(tuple._2)
-
-    Unmarshal(unzippedResponse.entity).to[SOReply].map(aps => doCalculate(aps.copy(tag = Some(tuple._1))))
-//
-//    unzippedResponse.entity.dataBytes
-//      .via(Framing.delimiter(ByteString("\n"), Int.MaxValue, allowTruncation = true))
-//      .map(_.utf8String)
-//      .runWith(Sink.head[String])
-  }
-
   implicit def myExceptionHandler: ExceptionHandler =
     ExceptionHandler {
       case ex =>
         extractUri { uri =>
           println(s"Request to $uri could not be handled normally")
+          ex.printStackTrace()
           complete(HttpResponse(StatusCodes.InternalServerError, entity = s"Something wrong happens! ${ex.getMessage}"))
         }
     }
@@ -127,13 +143,12 @@ object Main extends App with JsonSupport {
     path("search") {
       parameters('tag.*, 'format ? false) { (tags, doPrettyPrint) =>
         get {
-          val responseFut1: Future[immutable.Seq[JsObject]] =
+          val responseFut: Future[immutable.Seq[JsObject]] =
             Source(tags.toList)
-            .mapAsyncUnordered(4)(queueRequest)
-            .mapAsyncUnordered(4)(extractResult)
-            .runWith(Sink.seq[JsObject])
+              .mapAsyncUnordered(8)(queueRequest)
+              .runWith(Sink.seq[JsObject])
 
-          onComplete(responseFut1) {
+          onComplete(responseFut) {
             case Failure(exception) =>
               complete(exception)
 
@@ -147,8 +162,8 @@ object Main extends App with JsonSupport {
   val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-  StdIn.readLine() // let it run until user presses return
+  StdIn.readLine()
   bindingFuture
-    .flatMap(_.unbind()) // trigger unbinding from the port
-    .onComplete(_ => system.terminate()) // and shutdown when done
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
 }
